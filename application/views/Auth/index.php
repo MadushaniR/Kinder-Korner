6fb0b6a08cee04812802ae298da5083abc0197a1
MadushaniR <madushani.2019360@iit.ac.lk>
2024-02-19 11:34:43 +0530

    buttons added

--- application/views/Auth/index.php
+++ application/views/Auth/index.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="utf-8">
 	<meta name="author" content="Muhamad Nauval Azhar">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<meta name="description" content="This is a login page template based on Bootstrap 5">
 	<title>Bootstrap 5 Login Page</title>
-    <link rel="stylesheet" href="<?php echo site_url();?>assets/all.css">
-    <link rel="stylesheet" href="<?php echo site_url();?>assets/toast/toast.min.css">
-    <script src="<?php echo site_url();?>assets/toast/jqm.js"></script>
-    <script src="<?php echo site_url();?>assets/toast/toast.js"></script>
-    
- </head>
+	<link rel="stylesheet" href="<?php echo site_url(); ?>assets/all.css">
+	<link rel="stylesheet" href="<?php echo site_url(); ?>assets/toast/toast.min.css">
+	<script src="<?php echo site_url(); ?>assets/toast/jqm.js"></script>
+	<script src="<?php echo site_url(); ?>assets/toast/toast.js"></script>
+
+</head>
 
 <body>
 	<section class="h-100">
@@ -19,51 +20,65 @@
 			<div class="row justify-content-sm-center h-100">
 				<div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
 					<div class="text-center my-5">
-						<img src="<?php echo site_url();?>assets/logo.jpg" alt="logo" width="100">
+						<img src="<?php echo site_url(); ?>assets/logo.jpg" alt="logo" width="100">
 					</div>
-				
-                    
 
-                    <div class="card text-white bg-primary">
-                      <img class="card-img-top" src="holder.js/100px180/" alt="">
-                      <div class="card-body">
-                        <h4 class="card-title">Welcome </h4>
-                        <p class="card-text">Users</p>
-                      </div>
-                    </div>
+					<div class="card text-white bg-primary">
+						<img class="card-img-top" src="holder.js/100px180/" alt="">
+						<div class="card-body">
+							<h4 class="card-title">Welcome </h4>
+							<p class="card-text">Users</p>
+						</div>
+					</div>
 
-					<div id="container">
+					<!-- <div id="container">
 	<h1>Welcome to Quiz!</h1>
-<form method ="" action="<?php echo base_url();?>index.php/Questions/quizdisplay">
+<form method ="" action="
+<?php echo base_url(); ?>index.php/Questions/quizdisplay">
     <input type ="submit" value = "start">
 </form>
-</div>
-					<div class="text-center mt-5 text-muted">
-						Copyright &copy; 2017-2021 &mdash; Your Company 
+</div> -->
+					<div id="container">
+						<h1>Welcome to Quiz!</h1>
+
+						<?php
+
+						$this->db->select('quizNumber');
+						$this->db->distinct();
+						$query = $this->db->get('quiz');
+
+						$uniqueQuizNumbers = $query->result_array();
+
+						foreach ($uniqueQuizNumbers as $quizNumber) {
+							echo '<form method="" action="' . base_url() . 'index.php/Questions/quizdisplay">';
+							echo '<input type="hidden" name="quizNumber" value="' . $quizNumber['quizNumber'] . '">';
+							echo '<input type="submit" value="Start Quiz ' . $quizNumber['quizNumber'] . '">';
+							echo '</form>';
+						}
+						?>
+
 					</div>
 				</div>
 			</div>
 		</div>
 	</section>
 
-    <script type="text/javascript">
+	<script type="text/javascript">
+		<?php if ($this->session->flashdata('suc')) { ?>
+			toastr.success("<?php echo $this->session->flashdata('suc'); ?>");
+		<?php } else if ($this->session->flashdata('wrong')) {  ?>
+			toastr.error("<?php echo $this->session->flashdata('wrong'); ?>");
+		<?php } else if ($this->session->flashdata('warning')) {  ?>
+			toastr.warning("<?php echo $this->session->flashdata('warning'); ?>");
+		<?php } else if ($this->session->flashdata('info')) {  ?>
+			toastr.info("<?php echo $this->session->flashdata('info'); ?>");
+		<?php } ?>
+		<?php
+		$this->session->unset_userdata('suc'); ?>
 
-
-<?php if($this->session->flashdata('suc')){ ?>
-    toastr.success("<?php echo $this->session->flashdata('suc'); ?>");
-<?php }else if($this->session->flashdata('wrong')){  ?>
-    toastr.error("<?php echo $this->session->flashdata('wrong'); ?>");
-<?php }else if($this->session->flashdata('warning')){  ?>
-    toastr.warning("<?php echo $this->session->flashdata('warning'); ?>");
-<?php }else if($this->session->flashdata('info')){  ?>
-    toastr.info("<?php echo $this->session->flashdata('info'); ?>");
-<?php } ?>
-<?php
-	$this->session->unset_userdata ( 'suc' ); ?>
-	
-	<?php
-    $this->session->unset_userdata ( 'wrong' ); ?>
-
-</script>
+		<?php
+		$this->session->unset_userdata('wrong'); ?>
+	</script>
 </body>
+
 </html>
