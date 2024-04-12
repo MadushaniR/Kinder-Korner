b627b401ab3f92b2bc99297e7075393da1a56dbf
MadushaniR <madushani.2019360@iit.ac.lk>
2024-04-12 18:01:13 +0530

    quizes display page styling added

--- application/views/Auth/index.php
+++ application/views/Auth/index.php
@@ -10,12 +10,54 @@
 	<link rel="stylesheet" href="<?php echo site_url(); ?>assets/toast/toast.min.css">
 	<script src="<?php echo site_url(); ?>assets/toast/jqm.js"></script>
 	<script src="<?php echo site_url(); ?>assets/toast/toast.js"></script>
+	<style>
+		body {
+			background-image: url(<?php echo base_url('assets/images/bg.jpg'); ?>);
+			background-size: cover;
+			background-position: center;
+		}
+
+		.play_quizes_img {
+			margin-left: 35%;
+			margin-right: auto;
+			top: 50px;
+			z-index: 1;
+
+		}
+
+		#quizSearch {
+			box-shadow: 0px 0px 6px 2px #ccffff;
+
+		}
+
+		.card-body {
+			background-color: #e6ffff;
+		}
+
+		.card-title {
+			text-align: center;
+			font-size: 30px;
+		}
+
+		.total-feedback {
+			text-align: center;
+		}
+
+		.play-quiz {
+			text-align: center;
+		}
+
+		.feedback {
+			text-align: center;
+		}
+	</style>
 </head>
 
 <body>
 	<header>
 		<?php $this->load->view('Comman/header'); ?>
 	</header>
+	<img src="<?php echo base_url('assets/images/play_quizzes.png'); ?>" alt="play quizes image" class="play_quizes_img">
 	<section class="h-100">
 		<div class="container h-100">
 			<div class="row justify-content-sm-center h-100">
@@ -33,22 +75,30 @@
 				$quizDetails = $query->result_array();
 
 				foreach ($quizDetails as $quiz) {
-					echo '<div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12 mb-3">';
+
+					echo '<div class="col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-12 mb-3">';
 					echo '<div class="card">';
 					echo '<div class="card-body">';
 					echo '<h5 class="card-title">' . $quiz['quizName'] . '</h5>';
 					echo '<form method="" action="' . base_url() . 'index.php/QuizDisplay/quizdisplay">';
 					echo '<input type="hidden" name="quizID" value="' . $quiz['quizID'] . '">';
+					echo '<div class="play-quiz">';
 					echo '<input type="submit" class="btn btn-primary" value="Start Quiz">';
+					echo '</div>';
 					echo '</form>';
 					echo '<div class="mt-2">';
-					echo '<button class="btn btn-success" onclick="updateCount(' . $quiz['quizID'] . ', \'like\')">Like</button>';
-					echo '<button class="btn btn-danger" onclick="updateCount(' . $quiz['quizID'] . ', \'dislike\')">Dislike</button>';
-					echo '<span id="quizCount_' . $quiz['quizID'] . '">Likes: ' . $quiz['totalLikes'] . ', Dislikes: ' . $quiz['totalDislikes'] . '</span>';
+					// echo '<div class="feedback">';
+					// echo '<button class="btn btn-success" onclick="updateCount(' . $quiz['quizID'] . ', \'like\')">Like</button>';
+					// echo '<button class="btn btn-danger" onclick="updateCount(' . $quiz['quizID'] . ', \'dislike\')">Dislike</button>';
+					// echo '</div>';
+					echo '<div class="feedback">';
+					echo '<img src="' . base_url('assets/images/like.png') . '" alt="Like" onclick="updateCount(' . $quiz['quizID'] . ', \'like\')" class="feedback-btn" style="width: 50px; height: 50px;">';
+					echo '<img src="' . base_url('assets/images/dislike.png') . '" alt="Dislike" onclick="updateCount(' . $quiz['quizID'] . ', \'dislike\')" class="feedback-btn" style="width: 50px; height: 50px;">';
+					echo '</div>';
 
-					// Display total likes and dislikes for each quiz
+					echo '<div class="total-feedback">';
 					echo '<div id="totalCount_' . $quiz['quizID'] . '">Total Likes: ' . $quiz['totalLikes'] . ', Total Dislikes: ' . $quiz['totalDislikes'] . '</div>';
-
+					echo '</div>';
 					echo '</div>';
 					echo '</div>';
 					echo '</div>';
